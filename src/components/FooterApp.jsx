54034eb3ebcdcd1451aa19ed16309362c21c7fe2
Pablo Marino <pmarino2013@gmail.com>
2025-11-20 08:57:48 -0300

    Agrego componentes básicos

---
+++ src/components/FooterApp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterApp = () => {
+  return <div>FooterApp</div>;
+};
+
+export default FooterApp;
---
+++ src/components/NavBarApp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBarApp = () => {
+  return <div>NavBarApp</div>;
+};
+
+export default NavBarApp;

d375a0ede660880f1e22a490f82fa2e634c4a153
juanmv32 <juan.m.vargas32@gmail.com>
2025-11-21 22:11:23 -0300

    se inicia el 404

--- src/views/Error404Screen.jsx
+++ src/views/Error404Screen.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error404Screen = () => {
-  return <div>Error404Screen</div>;
+  return (
+    <div className="container d-flex flex-column justify-content-center align-items-center text-center min-vh-100">
+      <h1 className="display-4 fw-bold">404</h1>
+      <p className="lead">
+        La página que estás buscando no existe o aún no está disponible😒.
+      </p>
+
+      <Link to="/" className="btn btn-dark mt-3">
+        Volver al inicio
+      </Link>
+    </div>
+  );
 };
 
 export default Error404Screen;

a278cbb7c4e849587ba7c3e6264d7182339bba4b
Facundo Vera <facundovera.dev@gmail.com>
2025-11-22 23:24:15 +0000

    Avance con la lógica del formulario agregando 5 usuarios y función handleChange para manejar inputs del formulario

--- src/views/LoginScreen.jsx
+++ src/views/LoginScreen.jsx
@@ -1,31 +1,76 @@
+import { useState } from "react";
 import "../css/login.css";
 
 const LoginScreen = () => {
+  const [formValue, setFormValue] = useState({
+  correo: "",
+  password: ""
+});
+  const handleChamge=(e)=> setFormValue({...formValue,[e.target.name]: e.target.value})
+  const usuarios = [
+    {
+      id: 1,
+      correo: "lucas@gmail.com",
+      password: "123456",
+      rol: "admin",
+      nombre: "Lucas"
+    },
+    {
+      id: 2,
+      correo: "maria@gmail.com",
+      password: "contra2025",
+      rol: "usuario",
+      nombre: "María"
+    },
+    {
+      id: 3,
+      correo: "facundo@gmail.com",
+      password: "pass789",
+      rol: "editor",
+      nombre: "Facundo"
+    },
+    {
+      id: 4,
+      correo: "sofia@gmail.com",
+      password: "abc123",
+      rol: "usuario",
+      nombre: "Sofía"
+    },
+    {
+      id: 5,
+      correo: "juan@gmail.com",
+      password: "juanito",
+      rol: "moderador",
+      nombre: "Juan"
+    }
+  ];
   return (
     <div className="container-fluid fondo">
       <div className="row vh-100  align-items-center">
         <div className="col-12 col-md-6 offset-md-3  p-4 rounded-5 form-fondo">
           <h1 className="text-center my-5">Inicia tu sesión</h1>
-            <form className="">
-              <div className="mb-3">
-                <label>Correo electrónico</label>
-                <input type="email" className="form-control" name="correo" />
-              </div>
-              <div className="mb-3">
-                <label>Contraseña</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                />
-              </div>
-              <div className="mb-3 d-grid">
-                <button className="btn btn-success">Iniciar</button>
-              </div>
-            </form>
-          </div>
+          <form className="">
+            <div className="mb-3">
+              <label>Correo electrónico</label>
+              <input type="email" className="form-control" name="correo"  onChange={handleChamge} />
+            </div>
+            <div className="mb-3">
+              <label>Contraseña</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                onChange={handleChamge}
+              
+              />
+            </div>
+            <div className="mb-3 d-grid">
+              <button className="btn btn-success">Iniciar</button>
+            </div>
+          </form>
         </div>
       </div>
+    </div>
   );
 };
 

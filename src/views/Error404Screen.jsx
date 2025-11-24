0636d911d74eb6142862456d0065055471dc4242
juanmv32 <juan.m.vargas32@gmail.com>
2025-11-24 20:55:45 -0300

    se modifica 404

--- src/views/Error404Screen.jsx
+++ src/views/Error404Screen.jsx
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 const Error404Screen = () => {
   return (
     <div className="container d-flex flex-column justify-content-center align-items-center text-center min-vh-100">
-      <h1 className="display-4 fw-bold">404</h1>
+      {/* <h1 className="display-4 fw-bold">404</h1> */}
+      <div>
+      <img src="src/assets/404.webp" className="img-fluid" /* style={{margin:"-100px"}} */ alt="" />
+
       <p className="lead">
-        La página que estás buscando no existe o aún no está disponible😒.
+        Al parecer hubo un problema con tu pedido😢.
       </p>
+      </div>
 
       <Link to="/" className="btn btn-dark mt-3">
         Volver al inicio
